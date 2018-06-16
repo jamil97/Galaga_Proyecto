9322c4d01d6c3811745432d1ff29510a6fd26e21
Jamil <jamvilleda97@live.com>
2018-06-16 14:05:36 -0600

    Clase Reactor Arc Funcional

---
+++ ReactorArc.h
@@ -0,0 +1,15 @@
+#ifndef REACTORARC_H
+#define REACTORARC_H
+#include <iostream>
+class ReactorArc{
+
+  public:
+    int x;
+    int y;
+
+  public:
+    ReactorArc();
+    ReactorArc(int,int);
+    bool navePrincipal();
+};
+#endif
